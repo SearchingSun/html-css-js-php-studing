baac8a4798401c1a32c31b9ef01989925274f97f
SearchingSun <andres556@yandex.ru>
2018-10-28 19:26:27 +0300

    trouble

---
+++ js-std/js/js5.js
@@ -0,0 +1,15 @@
+"use strict"
+//let cells;
+//cells = prompt("Поле - квадрат N x N, введите N", );
+//console.log(cells)
+
+//var div = document.createElement("div")
+//var mainDiv = document.getElementById("mainDiv");
+//mainDiv.appendChild(div)
+
+var parent = document.getElementById('parent');
+
+var p = document.createElement('p');
+p.innerHTML = 'Содержимое абзаца';
+
+parent.appendChild(p);
