84400a70a4f695c4ee081930c7d0b449133fd618
SearchingSun <andres556@yandex.ru>
2018-11-09 21:10:59 +0300

    Sad, I'm SO sad

--- js-std/js/sad/apple.js
+++ js-std/js/sad/apple.js
@@ -1,12 +1,13 @@
 class Apple{
 	constructor(name){
-		let garden_age = 0
+
 		this._appleName = name
-		this._age =  this.randomizer(1,31) + garden_age;
+		this._age = this.age();
 		this._color = this.color(this._age);
 		this._size = this.size(this._color);
 		this._GB = this.GB(this._age);
 		this._floorBranch = this.floorBranch(this._age);
+		this._daysOnFloor = 0;
 	};
 	set sAppleAge(_age){
 		setAppleAge = this._age
@@ -14,13 +15,18 @@ class Apple{
 	get gAppleAge(){
 		return this._age
 	}
+	age(){
+		if (this._age === undefined){
+			return this.randomizer(1,31);
+		}else return this._age            //????? а надо ли это ????
+	}
 	randomizer(min,max){
 	return Math.round(Math.random() * (max - min) + min);
 	}
 	color(age){
-		if (age <= 13 ){
+		if (this._age <= 13 ){
 			return 'Зелёное'
-		}else if ( 13 < age && age < 22 ){
+		}else if ( 13 < this._age && this._age < 22 ){
 			return 'Красно-зелёное'
 		}else return 'Красное'
 	}
@@ -33,25 +39,45 @@ class Apple{
 	}
 	GB(age){
 		if (this._GB === "Свежак" || this._GB === undefined){
-			if (Math.round(age*Math.random()) < 25){
+			if (Math.round(this._age*Math.random()) < 27){
 			return "Свежак"
 		}else return "Стухло"
 		}else return "Стухло"
 	}
 	floorBranch(age){
 		if (this._floorBranch === "На ветке" || this._floorBranch === undefined){
-			if (age >= 28 && age != 31){
-				if((Math.round(age*Math.random()) < 18)){
+			if (this._age >= 28 && this._age != 31){
+				if((Math.round(this._age*Math.random()) < 22)){
 					return "На ветке"
 				}else return "На земле"
-			}else if (age < 28){
+			}else if (this._age < 28){
 				return 'На ветке'
-			}else if (age === 31){
+			}else if (this._age === 31){
 				return "На земле"
 		}else return "На земле"
 	}
 }
-}
+	daysOnFloor(){
+		if(this._floorBranch === "На земле"){
+			return this._daysOnFloor +=0.5;
+			//Задать, через сколько дней испорченные яблоки на земле исчезнут
+		}else if (this._daysOnFloor === 5){
+			return true
+		}
+	}
+	newApple(derevoname) {
+		console.log("Ликуйте, смерды!!! У нас новое яблочко на "+ derevoname)
+		this._age = 0;
 
+	}
+	passAppleDay(){
+		this._age += 1
+		this._color = this.color(this._age);
+		this._size = this.size(this._color);
+		this._GB = this.GB(this._age);
+		this._floorBranch = this.floorBranch(this._age);
+
+}
+}
 
 
--- js-std/js/sad/derevo.js
+++ js-std/js/sad/derevo.js
@@ -2,14 +2,14 @@ class Derevo{
 	constructor(name){
 		this._derevoName = name
 		this._derevoAge = this.randomizer(1,30);
-		this._apples = this.apples()
+		this._apples = this.apples(12,30)
 	};
 	randomizer(min,max){
 	return Math.round(Math.random() * (max - min) + min);
 	}
-	apples(){
+	apples(fst, scd){
 		let apples = [];
-		let z = this.randomizer(12,30)
+		let z = this.randomizer(fst,scd)
 		for (let i = 0; i < z; i++) {
 			apples[i] = new Apple("Яблоко № " + (apples.length+1))
 		}
@@ -17,16 +17,45 @@ class Derevo{
 	}
 	addAples(quantity){
 		for (let k = 1; k <= quantity; k++) {
-			let p = this._apples.length + 1
+			let p = this._apples.length + 1;
 			this._apples[this._apples.length] = new Apple("Яблоко № "+p);
 		}
 		console.log("На дерево добавлены яблоки в количестве " + quantity+ " штук")
 		return this._apples
 	}
 	passDays(){
-		console.log("Дерево постарело на сутки")
-		if ((this._derevoAge % 30) === 0)
-			console.log("+яблоко")
+		// Можно регулировать частоту появления яблок, заменяя знаение chastota !!
+		console.log("Милорд, Деревья стареют")
+		let chastota = 30
+		if ((this._derevoAge % chastota) === 0){
+			let q = this._apples.length+1;
+			this._apples[this._apples.length] = new Apple("Яблоко № "+q);
+			this._apples[this._apples.length-1].newApple(this._derevoName)
+		}
+		for (let i = 0; i < this._apples.length; i++) {
+				this._apples[i].passAppleDay();
+				this._apples[i].daysOnFloor();
+			}
 		return this._derevoAge += 1
 	}
+	newDerevo(){
+		this._derevoAge = 1;
+		this._apples = this.apples(0,0);
+	}
+	kolvoApples(G){
+		let app = 0;
+		for (let i = 0; i < this._apples.length; i++) {
+			if(this._apples[i]._GB === G || this._apples[i]._size === G  || this._apples[i]._floorBranch === G || this._apples[i]._color === G){
+			app += 1
+			}
+	}
+	return app
+	}
+	chistka(){
+		for (let i = 0; i < this._apples.length; i++) {
+		if( this._apples[i].daysOnFloor() === true){
+		this._apples.splice(this._apples.indexOf(this._apples[i]),1)
+	}
+	}
+}
 }
