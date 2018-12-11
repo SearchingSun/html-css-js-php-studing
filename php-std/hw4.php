56f7bdd51e1037e0e85423aaf8bfa754fae61331
SearchingSun <andres556@yandex.ru>
2018-12-11 17:30:14 +0300

    hw4 first zadanie

--- php-std/hw4.php
+++ php-std/hw4.php
@@ -8,33 +8,35 @@
 // ЗАГРУЗКА ФАЙЛОВ НА СЕРВЕР
 $files = $_FILES; // хранит всю информацию о загруженных файлах
 var_dump($files['picture']);
+$maxSize = 777777; //Максимально возможный размер изображения менять тута.
 
-$name = $files['picture']['name'];
-$tmp_name = $files['picture']['tmp_name'];
-// md5();
-//md5_file();
-$x = $files['file']['name'[0]];
-move_uploaded_file($files['file']['tmp_name'[0]], "serverFileBase/$x");
+for ($i = 0; $i<count($files['picture']['name']);$i++) {
+    $size = $files['picture']['size'][$i];
+    $type = $files['picture']['type'][$i];
 
-
-
-//Версия 2
-//$path = 'serverFileBase/';
-//if ($_SERVER['REQUEST_METHOD'] == 'POST')
-//{
-//    if (!@copy($_FILES['picture']['tmp_name'], $path . $_FILES['picture']['name']))
-//        echo 'Что-то пошло не так';
-//    else
-//        echo 'Загрузка удачна';
-//}
-//
+    if ($size <= $maxSize && ($type === 'image/jpeg' || $type === "image/png")) {
+        $name = $files['picture']['name'][$i];
+        $tmp_name = $files['picture']['tmp_name'][$i];
+        move_uploaded_file($tmp_name, "serverFileBase/$name");
+    } else if ($size >= $maxSize && ($type === 'image/jpeg' || $type === "image/png")) {
+        var_dump("Изображение " . $files['picture']['name'][$i] . " превышает максимальный 
+        возможный размер изображения и не было загружено на сервер (Идите лесом, господа)");
+    } else if ($size <= $maxSize && ($type != 'image/jpeg' || $type != "image/png")) {
+        var_dump("Ты что, совсем бум-бум?. По русски же написано - только PNG и JPEG. А " .
+            $files['picture']['name'][$i] . "К ним не относится");
+    } else if ($size >= $maxSize && ($type != 'image/jpeg' || $type != "image/png")) {
+        var_dump("Я так не играю. И файл не тот и размер не пролазит. Дорогой пользователь, убейся АП стену вместе с " .
+            $files['picture']['name'][$i]);
+    }
+}
 
 
 ?>
 
 <form enctype="multipart/form-data"
       action="hw4.php" method="post">
-    <input name="picture[]" size="" type="file" multiple accept="image/*"> <br>
+    <p>Для загрузки на сервер доступны картинки размером менее <?php echo $maxSize ?> байт и расширений PNG  и JPEG</p>
+    <input name="picture[]" size="" type="file" multiple> <br>
     <input type="submit" value="Send">
 </form>
 
